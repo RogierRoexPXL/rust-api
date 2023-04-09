3eda067512b05c72e54d98a0680b0643dd0dbbe1
Rogier Roex <12000409@student.pxl.be>
2023-04-09 16:12:20 +0200

    checking in

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod model;
 mod repository;
 
 use api::transactions;
+use api::helloworld;
 
 use actix_web::{App, HttpServer};
 
@@ -11,6 +12,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(transactions::get_transactions)
+            .service(helloworld::hello)
     })
     .bind("127.0.0.1:8080")?
     .run()
--- src/repository/postgresdb.rs
+++ src/repository/postgresdb.rs
@@ -1,15 +1,36 @@
+use std::env;
 use tokio_postgres::{NoTls, Error};
+use env_logger::Builder;
+use log::{error, info};
 
 pub async fn establish_connection() -> Result<tokio_postgres::Client, Error> {
-    let (client, connection) = tokio_postgres::connect(
-        "host=localhost port=5432 dbname=transaction_db user=postgres password=pgdbRogier22",
-        NoTls,
-    )
-    .await?;
+    // Set up the logger
+    let mut builder = Builder::from_default_env();
+    builder.filter_module("tokio_postgres", log::LevelFilter::Debug);
+    builder.init();
+
+    // Get the database connection details from environment variables
+    let host = env::var("PGHOST").unwrap_or_else(|_| "localhost".to_string());
+    let port = env::var("PGPORT").unwrap_or_else(|_| "5432".to_string());
+    let database = env::var("PGDATABASE").unwrap_or_else(|_| "postgres".to_string());
+    let user = env::var("PGUSER").unwrap_or_else(|_| "postgres".to_string());
+    let password = env::var("PGPASSWORD").unwrap_or_else(|_| "password".to_string());
+
+    // Configure the database connection
+    let conn_string = format!(
+        "host={} port={} dbname={} user={} password={}",
+        host, port, database, user, password
+    );
+    let (client, connection) = tokio_postgres::connect(&conn_string, NoTls).await?;
+
+    // Log the successful connection
+    info!("Connected to database: {}", conn_string);
+
     tokio::spawn(async move {
         if let Err(e) = connection.await {
-            eprintln!("connection error: {}", e);
+            error!("Connection error: {} with username {} and password {}", e, user, password);
         }
     });
+
     Ok(client)
 }
